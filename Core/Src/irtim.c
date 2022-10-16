464f722e1775383c753df88c27f11ff5b67d1918
Weronika Kamińska <weronikaka99@gmail.com>
2022-10-16 16:19:41 +0200

    Add new files

---
+++ Core/Src/irtim.c
@@ -0,0 +1,48 @@
+/**
+  ******************************************************************************
+  * @file    irtim.c
+  * @brief   This file provides code for the configuration
+  *          of the IRTIM instances.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "irtim.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/* IRTIM init function */
+void MX_IRTIM_Init(void)
+{
+
+  /* USER CODE BEGIN IRTIM_Init 0 */
+
+  /* USER CODE END IRTIM_Init 0 */
+
+  /* USER CODE BEGIN IRTIM_Init 1 */
+
+  /* USER CODE END IRTIM_Init 1 */
+  /* USER CODE BEGIN IRTIM_Init 2 */
+
+  /* USER CODE END IRTIM_Init 2 */
+
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
